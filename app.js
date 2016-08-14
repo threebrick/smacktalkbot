9ffd6988091df9c1548484a7b054267124edab46
threebrick <darnell@threebrick.com>
2016-08-14 16:59:05 -0400

    Updated app.js with id and pass

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
-var appId = process.env.MY_APP_ID || "Missing your app ID";
-var appSecret = process.env.MY_APP_SECRET || "Missing your app secret";
+var appId = process.env.MY_APP_ID || "dd0a4eb6-9324-4d4a-9251-da27f780a679";
+var appSecret = process.env.MY_APP_SECRET || "QcAK5MpmPiAXD2mqp1MSZ4c";
 
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot
